55293d9b8d11ccd52066a8b9045255b6f1da0143
Damian Janowski <damian.janowski@gmail.com>
2009-03-16 15:07:38 -0300

    Initial commit.

---
+++ lib/contest.rb
@@ -0,0 +1,38 @@
+require "test/unit"
+
+class Test::Unit::TestCase
+  def self.setup(&block)
+    setup_blocks << block
+  end
+
+  def setup
+    self.class.setup_blocks.each do |block|
+      instance_eval(&block)
+    end
+  end
+
+  def self.context(name, &block)
+    subclass = Class.new(self.superclass)
+    subclass.setup_blocks.unshift(*setup_blocks)
+    subclass.class_eval(&block)
+    const_set(context_name(name), subclass)
+  end
+
+  def self.test(name, &block)
+    define_method(test_name(name), &block)
+  end
+
+private
+
+  def self.setup_blocks
+    @setup_blocks ||= []
+  end
+
+  def self.context_name(name)
+    "#{name.gsub(/(^| )(\w)/) { $2.upcase }}Test".to_sym
+  end
+
+  def self.test_name(name)
+    "test_#{name.gsub(/\s+/,'_')}".to_sym
+  end
+end
---
+++ test/contest_test.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + "/../lib/contest"
+
+class FooTest < Test::Unit::TestCase
+  setup do
+    @value = 1
+  end
+
+  test "truth" do
+    assert_equal 1, @value
+  end
+
+  context "some context" do
+    setup do
+      @value += 1
+    end
+
+    test "another truth" do
+      assert_equal 2, @value
+    end
+
+    context "and a nested context" do
+      setup do
+        @value += 1
+      end
+
+      test "more" do
+        assert_equal 3, @value
+      end
+    end
+  end
+
+  context "some other context" do
+    setup do
+      @value += 1
+    end
+
+    test "yet another truth" do
+      assert_equal 2, @value
+    end
+  end
+end
